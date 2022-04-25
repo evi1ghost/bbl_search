1940c775625a1b99227dcf7a555e7e4f18e9497f
Дубинчик Андрей <evil_ghost@bk.ru>
2022-04-25 23:12:56 +0300

    add error handler

--- init_app.sh
+++ init_app.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+function errorHandler {
+    eval "$@"
+    local status=$? 
+    if (( status != 0 )); then
+        echo "Error occurred. Initialization failed."
+        exit 1
+    fi
+}
+
+
 echo "Creating Python3 virtual environment"
-python3 -m venv venv
+errorHandler "python3 -m venv venv"
 source venv/bin/activate
 echo "Installing app"
-pip install --upgrade pip
-pip install --editable .
+errorHandler "pip install --upgrade pip"
+errorHandler "pip install --editable ."
 
 echo "Creating db tables"
-./src/db.py
+errorHandler "./src/db.py"
 
 echo "Load data to database. This may take a few minutes."
-./src/load_data.py
+errorHandler "./src/load_data.py"
 echo "Done"
 echo 'Activate virtual environment: ". venv/bin/activate" and then enter "bbl_search --help" to see help message'
